ec6d60e4b8e802705398942807c11b1e5019f6e0
seonseon042 <seonseon042@gmail.comgit>
2024-09-26 14:41:49 +0900

    콘솔로그,리턴차이

---
+++ 연습문제/consoleLogReturn.js
@@ -0,0 +1,21 @@
+function first() {
+    let message = "코드잇"; 
+    return message;
+}
+
+
+function second() {
+    let message = "codeit";
+    console.log(message);
+}
+
+function third() {
+    let message = undefined;
+    console.log(message);
+}
+
+
+// 테스트 코드
+console.log(first());
+second();
+console.log(third());
