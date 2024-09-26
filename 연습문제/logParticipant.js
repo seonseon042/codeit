d57e49d1a01330b30e7560b5bb6df0b0f4a8a659
seonseon042 <seonseon042@gmail.comgit>
2024-09-26 14:19:55 +0900

    함수실습1

---
+++ 연습문제/logParticipant.js
@@ -0,0 +1,11 @@
+// 여기에 코드를 작성하세요
+function logParticipant(name){
+    console.log(`${name}(이)가 대화에 참여했습니다.`)
+  }
+  // 테스트 코드
+  logParticipant('동수');
+  logParticipant('윤하');
+  logParticipant('재준');
+  logParticipant('동훈');
+  logParticipant('영희');
+  logParticipant('신욱');
