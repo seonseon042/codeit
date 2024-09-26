b2f8e8aa90e4fe0c2f5350d1e65971f644d64a33
seonseon042 <seonseon042@gmail.comgit>
2024-09-26 14:24:40 +0900

    함수실습2

---
+++ 연습문제/expressMultiplication.js
@@ -0,0 +1,11 @@
+// 여기에 코드를 작성하세요
+function expressMultiplication(a, b){
+    console.log(`${a} * ${b} = `+ a * b)
+  }
+  // 테스트 코드
+  expressMultiplication(3, 4);
+  expressMultiplication(3, 2);
+  expressMultiplication(7, 5);
+  expressMultiplication(8, 9);
+  expressMultiplication(5, 5);
+  expressMultiplication(9, 9);
